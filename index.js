2effdf6859260efa0b2f2840a01d4339cf3c1cdf
pravinbangar21 <54072844+pravinbangar21@users.noreply.github.com>
2022-10-13 01:01:21 +0530

    f

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App Pravin!')
+  res.send('CICD App Pravin22!')
 })
 
 app.get('/status', (req, res) => {
